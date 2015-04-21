4479b765acf1dbcc774c0097646f906c38455a82
Shu <chugen.shu@weblio.co.jp>
2015-04-21 11:00:58 +0900

    -

--- deepy/util/detect_nan.py
+++ deepy/util/detect_nan.py
@@ -5,11 +5,11 @@ import theano
 import numpy as np
 
 def detect_nan(i, node, fn):
-            for output in fn.outputs:
-                if (not isinstance(output[0], np.random.RandomState) and
-                    np.isnan(output[0]).any()):
-                    print '*** NaN detected ***'
-                    theano.printing.debugprint(node)
-                    print 'Inputs : %s' % [input[0] for input in fn.inputs]
-                    print 'Outputs: %s' % [output[0] for output in fn.outputs]
-                    import pdb;pdb.set_trace()
+    for output in fn.outputs:
+        if (not isinstance(output[0], np.random.RandomState) and
+            np.isnan(output[0]).any()):
+            print '*** NaN detected ***'
+            theano.printing.debugprint(node)
+            print 'Inputs : %s' % [input[0] for input in fn.inputs]
+            print 'Outputs: %s' % [output[0] for output in fn.outputs]
+            import pdb;pdb.set_trace()
