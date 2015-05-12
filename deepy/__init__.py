c0836b811ce8fb9e6191638ba0fa9c72f56c695a
Shu <chugen.shu@weblio.co.jp>
2015-05-12 15:06:33 +0900

    version up

--- deepy/__init__.py
+++ deepy/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from networks import *
 from conf import *
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     license='MIT',
     platforms='any',
 
-    packages=find_packages(),
+    packages=filter(lambda s: "secret" not in s, find_packages()),
 
     classifiers=[ # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 2 - Pre-Alpha',
