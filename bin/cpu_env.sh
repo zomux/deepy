20ac1ebeb8af6d609c1e5e47c04d0ca1ee8ac5ab
Shu <chugen.shu@weblio.co.jp>
2015-05-26 14:24:49 +0900

    Speed up with inplace options

--- bin/cpu_env.sh
+++ bin/cpu_env.sh
@@ -2,7 +2,7 @@
 
 # It turns out 'openmp=True' will cause some problems in Theano 0.7.0, so it was removed
 
-export THEANO_FLAGS='mode=FAST_RUN,floatX=float32,optimizer_excluding=inplace,allow_gc=False'
+export THEANO_FLAGS='mode=FAST_RUN,floatX=float32,openmp_elemwise_minsize=1000,allow_gc=False'
 export OMP_NUM_THREADS=`nproc`
 export PYTHONPATH="$PYTHONPATH:."
 
