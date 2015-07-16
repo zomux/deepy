7ab39426aa36d9e189815ac3638be2859f29862b
Ubuntu <ubuntu@ip-10-0-0-156.ec2.internal>
2015-07-16 08:25:28 +0000

    Switch back norm computation

--- deepy/trainers/util.py
+++ deepy/trainers/util.py
@@ -21,9 +21,9 @@ def multiple_l2_norm(tensors):
     Get the L2 norm of multiple tensors.
     This function is taken from blocks.
     """
-    # flattened = [T.as_tensor_variable(t).flatten() for t in tensors]
-    # flattened = [(t if t.ndim > 0 else t.dimshuffle('x'))
-    #              for t in flattened]
-    # joined = T.join(0, *flattened)
-    # return T.sqrt(T.sqr(joined).sum())
-    return T.sqrt(sum(T.sum(t ** 2) for t in tensors))
+    flattened = [T.as_tensor_variable(t).flatten() for t in tensors]
+    flattened = [(t if t.ndim > 0 else t.dimshuffle('x'))
+                 for t in flattened]
+    joined = T.join(0, *flattened)
+    return T.sqrt(T.sqr(joined).sum())
+    # return T.sqrt(sum(T.sum(t ** 2) for t in tensors))
