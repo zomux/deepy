5c54f3bd1c8edd7df9763c6f82b2beeab403daf7
Shu <chugen.shu@weblio.co.jp>
2016-02-25 16:51:03 +0900

    Fixed tests

--- test/test_auto_encoder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from func import run_experiment, ERROR_KEYWORD
-
-
-def test():
-    result = run_experiment("highway_networks/mnist_highway.py", timeout=60)
-    assert ERROR_KEYWORD not in result
