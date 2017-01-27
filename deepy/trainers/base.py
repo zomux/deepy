c50b4cabc84128c85e9563273357e57725114c82
Raphael Shu <raphael@uaca.com>
2017-01-28 00:01:21 +0900

    -

--- deepy/trainers/base.py
+++ deepy/trainers/base.py
@@ -99,7 +99,7 @@ class NeuralTrainer(object):
         """
         Get current epoch.
         """
-        return self._epoch + 1
+        return self._epoch
 
     def _setup_costs(self):
         self.cost = self._add_regularization(self.network.cost)
@@ -384,7 +384,7 @@ class NeuralTrainer(object):
         """
         Get current epoch.
         """
-        return self._epoch + 1
+        return self._epoch
 
 
     def get_data(self, data_split="train"):
