e3caf3361ee1474a83a5c6f6251bd51335619da2
Raphael Shu <raphael@uaca.com>
2017-02-21 10:11:58 +0900

    Fixed dropout

--- deepy/core/__init__.py
+++ deepy/core/__init__.py
@@ -4,5 +4,5 @@
 from tensor_conversion import convert_to_neural_var, convert_to_theano_var, neural_computation, neural_computation_prefer_tensor
 from env import env
 from graph import graph
-from runtime import runtime
+from runtime_class import runtime
 from loop import Loop
